5c29cb159cd95039e20822234c17ba96f6cdebb7
pwlin <pwlin05@gmail.com>
2013-06-14 21:37:56 +0200

    skip 'done-dir' directory

--- pub/clean-srt/rename-files.php
+++ pub/clean-srt/rename-files.php
@@ -4,32 +4,33 @@ function init($libraries=array()){
     $root_folder = '/cygdrive/z';
     foreach (glob($root_folder . '/*') as $foldername) {
         echo "$foldername\n";
-		if ($foldername == 'done-dir') {
+		if ($foldername == $root_folder . '/done-dir') {
 			continue;
-		}
-        foreach (glob($foldername . '/*') as $filename) {
-            if (is_dir($filename)) {
-                deltree($filename);
-            } elseif (preg_match('/\.jpg$|\.txt$|\.nfo$/i', $filename)) {
-                unlink($filename);
-            } else {
-                $original_file_name = explode('/', $filename);
-                $original_file_name = end($original_file_name);
+		} 
+		foreach (glob($foldername . '/*') as $filename) {
+			if (is_dir($filename)) {
+				deltree($filename);
+			} elseif (preg_match('/\.jpg$|\.txt$|\.nfo$/i', $filename)) {
+				unlink($filename);
+			} else {
+				$original_file_name = explode('/', $filename);
+				$original_file_name = end($original_file_name);
 
-                $path_info = pathinfo($original_file_name);
-                $extension = $path_info['extension'];
-                
-                $original_folder_name = explode('/', $foldername);
-                $original_folder_name = end($original_folder_name);
-                
-                $new_file_name = str_replace($original_file_name, $original_folder_name . '.' . $extension, $filename);
-                
+				$path_info = pathinfo($original_file_name);
+				$extension = $path_info['extension'];
+				
+				$original_folder_name = explode('/', $foldername);
+				$original_folder_name = end($original_folder_name);
+				
+				$new_file_name = str_replace($original_file_name, $original_folder_name . '.' . $extension, $filename);
+				
 				echo ("renaming $filename to $new_file_name\n");
-                rename($filename, $new_file_name);
-                
-            }
-        
-        }   
+				rename($filename, $new_file_name);
+				
+			}
+		
+		}
+	
     }
 
 }
